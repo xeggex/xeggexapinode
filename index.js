309bba2a19655e0843dd69c49f247b777416e17b
Xeggex <support@xeggex.com>
2021-12-24 13:18:28 +0000

    initial

---
+++ index.js
@@ -0,0 +1,33 @@
+import xeggexApi from './xeggexApi.js'
+
+const yourApiKey = "--your-api-key--";
+const yourApiSecret = "--your-api-secret--";
+
+const restapi = new xeggexApi(yourApiKey, yourApiSecret);
+
+
+
+(async () => {
+
+	
+
+	
+	//var balances = await restapi.getBalances();
+	//console.log(balances);
+	
+	//var openorders = await restapi.getCancelledOrders();
+	//console.log(openorders);
+	
+	//var tradehistory = await restapi.getTradeHistory();
+	//console.log(tradehistory);
+		
+	
+	//var neworder1 = await restapi.createLimitOrder('XGX/USDC', 'buy', 1, 0.01);
+	//console.log(neworder1)
+
+		
+
+
+})();
+
+
---
+++ xeggexApi.js
@@ -0,0 +1,219 @@
+/*
+	Xeggex API Class for NodeJS
+	version: 0.1.0
+*/
+
+import got from 'got';
+import Big from 'big.js';
+	
+class xeggexApi {
+		
+	constructor(apiKey = null, apiSecret = null, apiURL) 
+	{
+		if (apiURL === void 0)
+			this.apiURL = 'https://xeggex.com/api/v2';
+		else
+			this.apiURL = apiURL;
+	
+		if (apiKey != null && apiSecret != null)
+		{
+	
+			this.auth = "Basic " + Buffer.from(apiKey + ":" + apiSecret).toString("base64");
+		
+			this.options = {
+						prefixUrl: this.apiURL,
+						headers:{"Authorization" : this.auth}
+					};
+						
+		}
+		else
+		{
+
+			this.options = new Options({
+								prefixUrl: this.apiURL,
+							});
+		
+		}
+		
+		return this;
+	}
+	
+    getBalances()
+    {
+
+        return new Promise((resolve, reject) => {
+        
+        	(async () => {
+
+				try {
+            		const response = await got.get('balances', this.options).json();
+					resolve(response);
+				} catch (e) {
+                    reject(e);
+                }
+            
+            })();
+            
+        });
+
+    }	
+
+    getOpenOrders(symbol, limit = 50, skip = 0)
+    {
+
+        return new Promise((resolve, reject) => {
+        
+        	(async () => {
+
+				try {
+        			if (symbol == null) symbol = '';
+					limit = parseInt(limit);
+					skip = parseInt(skip);
+				
+            		const response = await got.get(`getorders?status=active&symbol=${symbol}&limit=${limit}&skip=${skip}`, this.options).json();
+					resolve(response);
+				} catch (e) {
+                    reject(e);
+                }
+            
+            })();
+            
+        });
+
+    }	
+    
+    getFilledOrders(symbol, limit = 50, skip = 0)
+    {
+
+        return new Promise((resolve, reject) => {
+        
+        	(async () => {
+
+				try {
+        			if (symbol == null) symbol = '';
+					limit = parseInt(limit);
+					skip = parseInt(skip);
+				
+            		const response = await got.get(`getorders?status=filled&symbol=${symbol}&limit=${limit}&skip=${skip}`, this.options).json();
+					resolve(response);
+				} catch (e) {
+                    reject(e);
+                }
+            
+            })();
+            
+        });
+
+    }	
+
+    getCancelledOrders(symbol, limit = 50, skip = 0)
+    {
+
+        return new Promise((resolve, reject) => {
+        
+        	(async () => {
+
+				try {
+        			if (symbol == null) symbol = '';
+					limit = parseInt(limit);
+					skip = parseInt(skip);
+				
+            		const response = await got.get(`getorders?status=cancelled&symbol=${symbol}&limit=${limit}&skip=${skip}`, this.options).json();
+					resolve(response);
+				} catch (e) {
+                    reject(e);
+                }
+            
+            })();
+            
+        });
+
+    }
+
+    getTradeHistory(symbol, limit = 50, skip = 0)
+    {
+
+        return new Promise((resolve, reject) => {
+        
+        	(async () => {
+
+				try {
+        			if (symbol == null) symbol = '';
+					limit = parseInt(limit);
+					skip = parseInt(skip);
+				
+            		const response = await got.get(`gettrades?symbol=${symbol}&limit=${limit}&skip=${skip}`, this.options).json();
+					resolve(response);
+				} catch (e) {
+                    reject(e);
+                }
+            
+            })();
+            
+        });
+
+    }
+
+    cancelOrder(orderid)
+    {
+
+        return new Promise((resolve, reject) => {
+        
+        	(async () => {
+
+				try {
+            		const response = await got.post('cancelorder', {...this.options, json: {id: orderid}}).json();
+					resolve(response);
+				} catch (e) {
+                    reject(e);
+                }
+            
+            })();
+            
+        });
+
+    }
+    
+    createLimitOrder(symbol, side, quantity, price, userProvidedId = null, strictValidate = false)
+    {
+
+        return new Promise((resolve, reject) => {
+        
+        	(async () => {
+
+				try {
+            		const response = await got.post('createorder', {...this.options, json: {symbol: symbol, side: side, quantity: quantity, type: 'limit', price: price, userProvidedId: userProvidedId, strictValidate: strictValidate}}).json();
+					resolve(response);
+				} catch (e) {
+                    reject(e);
+                }
+            
+            })();
+            
+        });
+
+    }
+
+    createMarketOrder(symbol, side, quantity, userProvidedId = null, strictValidate = false)
+    {
+
+        return new Promise((resolve, reject) => {
+        
+        	(async () => {
+
+				try {
+            		const response = await got.post('createorder', {...this.options, json: {symbol: symbol, side: side, quantity: quantity, type: 'market', userProvidedId: userProvidedId, strictValidate: strictValidate}}).json();
+					resolve(response);
+				} catch (e) {
+                    reject(e);
+                }
+            
+            })();
+            
+        });
+
+    }
+    
+};
+
+export default xeggexApi;
